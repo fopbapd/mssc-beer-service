cadb05606479ae74a1026effecb8bb3500daebaf
oleksandr.shumskyi <o.shumskyi@spd-ukraine.com>
2020-11-28 23:54:35 +0200

    Add JUnit5 tests for Beer Service Controller. Closes #4

--- src/main/java/org/fopbapd/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/org/fopbapd/msscbeerservice/web/controller/BeerController.java
@@ -2,6 +2,7 @@ package org.fopbapd.msscbeerservice.web.controller;
 
 import org.fopbapd.msscbeerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
-@RequestMapping("/api/v1/beer")
+@RequestMapping(value = "/api/v1/beer",
+        consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 @RestController
 public class BeerController {
     @GetMapping("/{beerId}")
