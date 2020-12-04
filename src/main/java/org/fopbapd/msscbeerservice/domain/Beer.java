e0096596955ec79bdc4735de452a77b88b1c2477
oleksandr.shumskyi <o.shumskyi@spd-ukraine.com>
2020-12-04 20:29:17 +0200

    Add JPA entity for Beer object. Downgrade Spring version to 2.3.5.RELEASE to make h2 embedded DB working. Closes #5

---
+++ src/main/java/org/fopbapd/msscbeerservice/domain/Beer.java
@@ -0,0 +1,45 @@
+package org.fopbapd.msscbeerservice.domain;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class Beer {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    UUID id;
+
+    String beerName;
+    String beerStyle;
+
+    @Column(unique = true)
+    Long upc;
+    BigDecimal price;
+
+    Integer minOnHand;
+    Integer quantityToBrew;
+
+    @Version
+    Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    Timestamp createdDate;
+    @UpdateTimestamp
+    Timestamp lastModifiedDate;
+}
