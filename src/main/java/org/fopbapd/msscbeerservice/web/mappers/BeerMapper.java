841d568fe5732ad7e953be40688c9594c0df66f6
oleksandr.shumskyi <o.shumskyi@spd-ukraine.com>
2020-12-04 22:34:30 +0200

    Add MapStruct for type conversion. Closes #10

---
+++ src/main/java/org/fopbapd/msscbeerservice/web/mappers/BeerMapper.java
@@ -0,0 +1,12 @@
+package org.fopbapd.msscbeerservice.web.mappers;
+
+import org.fopbapd.msscbeerservice.domain.Beer;
+import org.fopbapd.msscbeerservice.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+@Mapper(uses = {DateMapper.class})
+public interface BeerMapper {
+    Beer beerToBeerDto(BeerDto beerDto);
+
+    BeerDto beerDtoToBeer(Beer beer);
+}
