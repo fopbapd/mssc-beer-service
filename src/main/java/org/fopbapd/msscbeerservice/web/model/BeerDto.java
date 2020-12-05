f7d1f5cb2ddd4c682203256665a81c17204c212a
oleksandr.shumskyi <o.shumskyi@spd-ukraine.com>
2020-12-05 22:02:42 +0200

    Add Jackson Config to BeerDto. Closes #14

--- src/main/java/org/fopbapd/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/org/fopbapd/msscbeerservice/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package org.fopbapd.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,8 +24,10 @@ public class BeerDto {
     private UUID id;
     @Null
     private Integer version;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime createdDate;
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     @Null
     private OffsetDateTime lastModifiedDate;
     @NotBlank
@@ -36,6 +39,7 @@ public class BeerDto {
     @NotNull
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
